146c39481f88a97394cbc10dba9ec814df7fc32e
Diego Lopez <aud0l@hotmail.com>
2018-09-07 22:25:36 -0500

    Agregar función para cancelar viaje

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,22 +13,60 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
+"use strict";
 
-import * as functions from 'firebase-functions'
+import * as functions from "firebase-functions";
 
 // if you need to use the Firebase Admin SDK, uncomment the following:
-import * as admin from 'firebase-admin'
-admin.initializeApp()
+import * as admin from "firebase-admin";
+admin.initializeApp();
+
+interface Position {
+  latitude: number;
+  longitude: number;
+}
+
+enum TripState {
+  ARRIVED = "arrived",
+  BOARDED = "boarded",
+  CANCEL = "cancel",
+  FINALIZE = "finalize",
+  PENDING = "pending",
+  TAKED = "taked",
+  TRAVELING = "traveling"
+}
+
+interface Trip {
+  notifiedDrivers?: { [key: string]: boolean };
+  position: Position;
+  state: string;
+  userId: string;
+}
 
 const arrayToObject = (sources: string[]) => {
-    const target: { [key:string]: boolean } = {}
+  const target: { [key: string]: boolean } = {};
 
-    for (const current of sources) {
-        target[current] = true
-    }
+  for (const current of sources) {
+    target[current] = true;
+  }
 
+  return target;
+};
+
+function objectToArray(sources: { [key: string]: boolean }) {
+  const target = [];
+
+  if (sources) {
     return target
+  }
+
+  for (const key in sources) {
+    if (sources.hasOwnProperty(key)) {
+      target.push(key);
+    }
+  }
+
+  return target;
 }
 
 // Create and Deploy Cloud Function with TypeScript using script that is
@@ -36,36 +74,75 @@ const arrayToObject = (sources: string[]) => {
 //    cd functions
 //    npm run deploy
 
-export const notifyDrivers = functions.database.ref('/trips/{tripId}')
-    .onCreate(async (snapshot, context) => {
-        const original = snapshot.val()
-        const passengerId = original.userId
-        const tripId = context.params.tripId as string
+export const cancelTrip = functions.database
+  .ref("/trips/{tripId}")
+  .onUpdate((change, context) => {
+    const before = change.before.val() as Trip;
+    const after = change.after.val() as Trip;
+    const tripId = context.params.tripId as string;
 
-        console.log('Creating new trip', tripId, original)
+    if (before.state === after.state) {
+      console.log(`Trip state dind't change`, after.state);
+      return null;
+    }
 
-        const drivers = await admin.database()
-                             .ref('drivers')
-                             .limitToFirst(10)
-                             .once('value')
+    if (after.state === TripState.CANCEL) {
+      const updates = {};
 
-        const driversIds = []
-        
-        drivers.forEach(snapshot => {
-            driversIds.push(snapshot.key)
-            return true
-        })
-
-        const insertions = {}
-
-        for (const driverId of driversIds) {
-            insertions[`/tripsByDrivers/${driverId}/${tripId}`] = original
+      if (after.notifiedDrivers) {
+        for (const driverId of objectToArray(after.notifiedDrivers)) {
+          updates[`/tripsByDrivers/${driverId}/${tripId}/state`] =
+            TripState.CANCEL;
         }
+      }
 
-        insertions[`/trips/${tripId}/notifiedDrivers`] = arrayToObject(driversIds)
-        insertions[`/tripsByPassengers/${passengerId}/${tripId}`] = original
+      updates[`/tripsByPassengers/${after.userId}/${tripId}/state`] =
+        TripState.CANCEL;
 
-        console.log('Notify updates', insertions)
+      return admin
+        .database()
+        .ref()
+        .update(updates);
+    }
 
-        return admin.database().ref().update(insertions)
-    })
+    return null;
+  });
+
+export const notifyDrivers = functions.database
+  .ref("/trips/{tripId}")
+  .onCreate(async (snapshot, context) => {
+    const original = snapshot.val();
+    const passengerId = original.userId;
+    const tripId = context.params.tripId as string;
+
+    console.log("Creating new trip", tripId, original);
+
+    const drivers = await admin
+      .database()
+      .ref("drivers")
+      .limitToFirst(10)
+      .once("value");
+
+    const driversIds = [];
+
+    drivers.forEach(snap => {
+      driversIds.push(snap.key);
+      return true;
+    });
+
+    const insertions = {};
+
+    for (const driverId of driversIds) {
+      insertions[`/tripsByDrivers/${driverId}/${tripId}`] = original;
+    }
+
+    insertions[`/trips/${tripId}/notifiedDrivers`] = arrayToObject(driversIds);
+    insertions[`/tripsByPassengers/${passengerId}/${tripId}`] = original;
+
+    console.log("Notify updates", insertions);
+
+    return admin
+      .database()
+      .ref()
+      .update(insertions);
+  });
